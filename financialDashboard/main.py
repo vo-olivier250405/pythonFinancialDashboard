bdc2e6e0f07df7ffabc196559f873d6aaa16de03
vo_o <vo_o@etna-alternance.net>
2024-02-01 15:10:22 +0100

    [ADD] => Modification du code

--- financialDashboard/main.py
+++ financialDashboard/main.py
@@ -1,6 +1,3 @@
-
-# Imports
-import numpy as np
 import datetime as dt
 from yfinance import download
 from math import pi
@@ -11,20 +8,17 @@ from bokeh.models import TextInput, Button, DatePicker, MultiChoice
 
 
 def load_data(ticker1, ticker2, start, end):
-    """
-    """
     df1 = download(ticker1, start, end)
     df2 = download(ticker2, start, end)
     return df1, df2
 
 
 def plot_data(data, indicators, sync_axis=None):
-    """
-    """
     df = data
     gain = df.Close > df.Open
     loss = df.Open > df.Close
     width = 12 * 60 * 60 * 1000
+
     if sync_axis is not None:
         p = figure(x_axis_type="datetime",
                    tools="pan,wheel_zoom,box_zoom,reset,save",
@@ -47,35 +41,35 @@ def plot_data(data, indicators, sync_axis=None):
     return p
 
 
-def on_button_click(ticker1, ticker2, start, end, indicators):
-    """
-    """
+def on_button_click():
+    ticker1 = stock1_text.value
+    ticker2 = stock2_text.value
+    start = date_picker_from.value
+    end = date_picker_to.value
+    indicators = indicators_choice.value
+
     df1, df2 = load_data(ticker1, ticker2, start, end)
     p1 = plot_data(df1, indicators)
     p2 = plot_data(df2, indicators, sync_axis=p1.x_range)
-    curdoc().clear()
-    curdoc().add_root(layout)
-    curdoc().add_root(row(p1, p2))
+
+    layout.children = [stock1_text, stock2_text, date_picker_from,
+                       date_picker_to, indicators_choice, load_button,
+                       row(p1, p2)]
 
 
 stock1_text = TextInput(title="Stock 1")
 stock2_text = TextInput(title="Stock 2")
 date_picker_from = DatePicker(title="Start Date", value="2020-01-01", min_date="2000-01-01",
                               max_date=dt.datetime.now().strftime("%Y-%m-%d"))
-
 date_picker_to = DatePicker(title="End Date", value="2020-02-01", min_date="2000-01-01",
                             max_date=dt.datetime.now().strftime("%Y-%m-%d"))
-
 indicators_choice = MultiChoice(
     options=["100 Day SMA", "30 Day SMA", "Linear Regression Line"])
-
 load_button = Button(
     label="Load Data", button_type="success")
-load_button.on_click(lambda: on_button_click(stock1_text.value, stock2_text.value,
-                                             date_picker_from.value, date_picker_to.value,
-                                             indicators_choice.value))
+load_button.on_click(on_button_click)
+
 layout = column(stock1_text, stock2_text, date_picker_from,
                 date_picker_to, indicators_choice, load_button)
 
-curdoc().clear()
 curdoc().add_root(layout)
